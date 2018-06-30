71a753524860c80349ecb583875ac766b39d048e
roip890 <roip890@gmail.com>
2018-07-01 00:14:19 +0300

    geo

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,15 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+  li.text(message.from + ': ');
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
@@ -33,11 +42,13 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.')
   }
-  console.log(navigator);
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log('sadsada');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
     console.log(position);
-  }, function () {
-    alert('Unable to fetch location.')
+  }, function (err) {
+    alert('Unable to fetch location.', err)
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,6 +11,22 @@ describe('generateMessage', () => {
     expect(message).toInclude({
       from,
       text
+    });
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Roi';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=' + latitude + ',' + longitude;
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({
+      from,
+      url
     });
   });
 });
